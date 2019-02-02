7e5c638d57a9bb78a22580841ab6a7e0a76b59a9
amanda_gibson <gibson.amanda.mae@gmail.com>
2019-02-02 17:05:19 +0100

    edits to fix module errors

--- spec/fizz-buzz.spec.js
+++ spec/fizz-buzz.spec.js
@@ -1,7 +1,9 @@
 const { FizzBuzz } = require('./spec.helper')
 
 describe('Fizz Buzz', () => {
-    let fizzBuzz = new FizzBuzz
+    let fizzBuzz = new FizzBuzz({
+    })
+
     
     it('returns a number if no game rules are met', () => {
         expect(fizzBuzz.check(1)).to.eql(1);
